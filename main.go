0f2eecde80b5d245de178530201477a082a29806
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-11-24 17:47:02 -0500

    feat: :sparkles: Add an index.html to parse with go templates

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World from AWS EC2!\n")
-	fmt.Fprintf(w, "By: Mateo Pillajo :D\n")
-	fmt.Fprintf(w, "Made with Golang\n")
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, nil)
 }
 func main() {
 	http.HandleFunc("/", handler)
